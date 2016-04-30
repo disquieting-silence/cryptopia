861db80e7055f9b5b8120118a5165a99baf0952c
dsq <dsq.email>
2016-04-30 15:36:19 +1000

    direction with ps

--- src/Alien.purs
+++ src/Alien.purs
@@ -5,6 +5,12 @@ import Prelude
 
 foreign import data DOM :: !
 
-type CryptopiaApi = { getName :: String }
+type Point = { x :: Int, y :: Int }
+type KeyEvent = { which :: Int }
+type Bounds = { width :: Int, height :: Int }
+
+type CryptopiaApi = {
+  getNextPosition :: Point -> KeyEvent -> Bounds -> Point
+}
 
 foreign import doEverything :: forall eff. CryptopiaApi -> Eff (dom :: DOM | eff) Unit
--- src/Main.purs
+++ src/Main.purs
@@ -7,17 +7,11 @@ import Data.Maybe
 
 import Alien
 
-type Point = { x :: Int, y :: Int }
-type KeyEvent = { which :: Int }
-type Bounds = { width :: Int, height :: Int }
-
-cat :: Int -> Int
-cat x = x + 1
 
 main :: forall e. Eff (console :: CONSOLE, dom :: DOM | e) Unit
 main = do
   log "Hello sailor!"
-  doEverything ({ getName: "dog" })
+  doEverything ({ getNextPosition: getNextPosition })
 
 getDelta :: KeyEvent -> Maybe Point
 getDelta evt =
@@ -34,13 +28,3 @@ getNextPosition pt evt bounds =
       nextX = ((pt.x + delta.x) + bounds.width) `mod` bounds.width
       nextY = ((pt.y + delta.y) + bounds.height) `mod` bounds.height
   in { x: nextX, y: nextY }
-  -- var delta = (function () {
-  --   if (event.which === 37) return { x: -1, y: 0 };
-  --   else if (event.which === 39) return { x: +1, y: 0 };
-  --   else if (event.which === 38) return { x: 0, y: -1 };
-  --   else if (event.which === 40) return { x: 0, y: +1 };
-  --   else return { x: 0, y: 0 };
-  -- })();
-  --
-  -- var nextRow = ((row + delta.y) + NUM_ROWS) % NUM_ROWS;
-  -- var nextCol = ((column + delta.x) + NUM_COLS) % NUM_COLS;
