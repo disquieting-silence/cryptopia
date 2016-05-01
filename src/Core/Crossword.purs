35dedfc8c18734f97d20eb9f12a6187eda00cb50
dsq <dsq.email>
2016-05-02 09:12:37 +1000

    Crossword parsing

--- src/Core/Crossword.purs
+++ src/Core/Crossword.purs
@@ -1,15 +1,24 @@
 module Core.Crossword where
 
 import Data.Maybe
+import Prelude(map)
 
 data CrosswordSquare =
   Void |
   Empty { num :: Maybe Int } |
-  Full { num :: Maybe Int }
+  Full { content :: String, num :: Maybe Int }
 
+data Crossword = Crossword (Array (Array CrosswordSquare))
 
-data Crossword = Crossword (Array CrosswordSquare)
+parseSquare :: String -> CrosswordSquare
+parseSquare "*" = Void
+parseSquare "" = Empty { num: Nothing }
+parseSquare c = Full { content: c, num: Nothing }
 
+parseRow :: Array String -> Array CrosswordSquare
+parseRow = map parseSquare
 
 parse :: Array (Array String) -> Crossword
-parse rows = Crossword [ ]
+parse rows =
+  let parsed = map parseRow rows
+  in Crossword parsed
