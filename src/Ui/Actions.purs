08df56018e8aba5ce115108a6c783c567d0cd499
dsq <dsq.email>
2016-05-06 08:35:53 +1000

    delegating through to Ui.Navigation

--- src/Ui/Actions.purs
+++ src/Ui/Actions.purs
@@ -10,29 +10,10 @@ import Data.Maybe
 import Data.Array
 import Prelude(bind, pure, ($), id, negate, mod, (+))
 import Ui.Common
-
-processDirection :: KeyEvent -> Maybe Direction
-processDirection evt =
-  case evt.which of
-    37 -> Just West
-    38 -> Just North
-    39 -> Just East
-    40 -> Just South
-    _ -> Nothing
-
-processNavigation :: Crossword -> Maybe Direction -> Maybe CellIndex -> Maybe CellIndex
-processNavigation cword dir cellIndex = do
-  d <- dir
-  i <- cellIndex
-  pure $ Core.Navigation.navigate cword d i
+import Ui.Navigation
 
 processKeydown :: forall eff. Node -> Crossword -> KeyEvent -> Eff (dom :: DOM | eff) (Maybe Node)
-processKeydown container cword evt = do
-  let dir = processDirection evt
-  cellIndex <- Ui.Ui.readIndicesFromCell evt.target
-  newFocus <- pure $ processNavigation cword dir cellIndex
-  Data.Maybe.maybe (pure Nothing) (\i -> findAgain container i) newFocus
-
+processKeydown container cword evt = Ui.Navigation.processKeydown container cword evt
 
 modifySquare :: Int -> (CrosswordSquare -> CrosswordSquare)
 modifySquare 32 = Core.Crossword.toBlank
